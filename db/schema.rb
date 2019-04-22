66c6b1b60a71d952377cea4a38c0f75be3bfbb42
Cesar Cachay <cesarcachay1996@gmail.com>
2019-04-22 09:04:16 -0500

    Fix Dropdown

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_212006) do
+ActiveRecord::Schema.define(version: 2019_04_22_135358) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -26,10 +26,10 @@ ActiveRecord::Schema.define(version: 2019_04_21_212006) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "category_id"
-    t.string "play_img_file_name"
-    t.string "play_img_content_type"
-    t.bigint "play_img_file_size"
-    t.datetime "play_img_updated_at"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
